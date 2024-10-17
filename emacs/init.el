f18b2f3c778fd0e95ebe2732d666d9ac9aa8be60
Tobias <79578794+zhekemist@users.noreply.github.com>
2024-10-17 21:18:09 +0200

    Change some emacs settings

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,11 @@
 (setq-default tab-width 4)
+
 (load-theme 'solarized-dark t)
+
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+(setq-default org-list-demote-modify-bullet '(("+" . "-") ("-" . "*") ("*" . "+")))
