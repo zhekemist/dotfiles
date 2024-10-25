b102b90641c1a2728d5feb3a71649cfcd3bbc536
Tobias <79578794+zhekemist@users.noreply.github.com>
2024-10-25 11:31:35 +0200

    Fix emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -5,12 +5,11 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
+(require 'org)
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
-
 (setq-default org-list-demote-modify-bullet '(("+" . "-") ("-" . "*") ("*" . "+")))
-
 (add-hook 'org-mode-hook #'turn-on-org-cdlatex)
 (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.3))
 
