259ae6d530f41fdb25afa0f149e0ba9096c44fa0
Tobias <79578794+zhekemist@users.noreply.github.com>
2024-10-25 11:02:28 +0200

    Increase font size of latex previews

--- emacs/init.el
+++ emacs/init.el
@@ -8,8 +8,11 @@
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
+
 (setq-default org-list-demote-modify-bullet '(("+" . "-") ("-" . "*") ("*" . "+")))
+
 (add-hook 'org-mode-hook #'turn-on-org-cdlatex)
+(setq org-format-latex-options (plist-put org-format-latex-options :scale 1.3))
 
 (pdf-tools-install)
 
